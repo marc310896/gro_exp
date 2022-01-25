11937c3cc687ce85e67d5521f5a4b8f6ff901ede
marc310896 <mhoegler868@googlemail.com>
2022-01-25 20:15:30 +0100

    add area function

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from distutils.core import setup
 
-setup(
+import setuptools
+
+setuptools.setup(
     name="utils",
     version="0.0.1",
     description='description',
     author='Marc Hoegler',
     author_email='marc.hoegler@icvt.uni-stuttgart.de',
     classifiers=[],
-    #python_requires='>=3.5',
+    python_requires='>=3.5',
     url='https://github.com/marc310896/utils',
-    #install_requires=['numpy', 'pandas', 'seaborn'],
-    #include_package_data=True,
+    download_url='https://github.com/marc310896/utils/archive/refs/tags/0.0.1.tar.gz',
+    install_requires=['numpy', 'pandas', 'seaborn'],
+    include_package_data=True,
+    packages=setuptools.find_packages(),
 )
